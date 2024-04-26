9838635e9764d0b3b7219e7d020e4340476d7af3
joakha <72503684+joakha@users.noreply.github.com>
2024-04-26 13:52:20 +0300

    functionality for adding trainings

---
+++ src/components/AddTraining.jsx
@@ -0,0 +1,137 @@
+import { useState } from "react"
+import { Button, TextField, Dialog, DialogActions, DialogContent, DialogTitle, Select, MenuItem } from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers";
+
+const AddTraining = ({ addTraining }) => {
+
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [customers, setCustomers] = useState([]);
+    const [training, setTraining] = useState({
+        date: null,
+        duration: "",
+        activity: "",
+        customer: ""
+    })
+
+    const handleOpen = async () => {
+        setOpen(true);
+
+        try {
+            setLoading(true);
+            const response = await fetch("https://customerrestservice-personaltraining.rahtiapp.fi/api/customers");
+            const customersData = await response.json();
+            setCustomers(customersData._embedded.customers);
+            setLoading(false)
+        }
+
+        catch (error) {
+            console.error(error);
+        }
+    }
+
+    const handleDate = (value) => {
+        setTraining({ ...training, date: value });
+    }
+
+    const handleFieldChange = (event) => {
+        setTraining({ ...training, [event.target.name]: event.target.value });
+    }
+
+    const handleSelect = (event) => {
+        setTraining({ ...training, customer: event.target.value });
+    }
+
+    const handleSave = () => {
+        addTraining(training);
+        setOpen(false);
+        setTraining(
+            {
+                date: null,
+                duration: "",
+                activity: "",
+                customer: ""
+            }
+        );
+    }
+
+    const handleCancel = () => {
+        setOpen(false);
+        setTraining(
+            {
+                date: null,
+                duration: "",
+                activity: "",
+                customer: ""
+            }
+        );
+    }
+
+    return (
+
+        <>
+            <Button variant="contained" style={{ marginTop: "5px", marginBottom: "20px", backgroundColor: "rgb(0, 204, 153)" }} onClick={handleOpen}>Add a Training</Button>
+            {
+                loading ? <p>Loading training form...</p> :
+
+                    <Dialog open={open}>
+                        <DialogTitle>Add a new Training</DialogTitle>
+                        <DialogContent>
+                            <DatePicker
+                                value={training.date}
+                                onChange={handleDate}
+                                format="DD-MM-YYYY"
+                                label="Date"
+                                sx={{ marginTop: 1 }}
+                            />
+
+                            <TextField
+                                label="Duration in minutes"
+                                name="duration"
+                                value={training.duration}
+                                margin="dense"
+                                onChange={handleFieldChange}
+                                variant="standard"
+                                fullWidth>
+                            </TextField>
+
+                            <TextField
+                                label="Activity"
+                                name="activity"
+                                value={training.activity}
+                                margin="dense"
+                                onChange={handleFieldChange}
+                                variant="standard"
+                                fullWidth>
+                            </TextField>
+
+                            <Select
+                                value={training.customer}
+                                sx={{marginTop: 3}}
+                                onChange={handleSelect}
+                                displayEmpty
+                            >
+
+                                <MenuItem value="" disabled>Select a customer</MenuItem>
+                                {customers.map((customer, index) => {
+
+                                    return <MenuItem key={index} value={customer._links.self.href}>{`${customer.firstname} ${customer.lastname}`}</MenuItem>
+
+                                })}
+
+                            </Select>
+                        </DialogContent>
+
+                        <DialogActions>
+                            <Button onClick={handleCancel}>Cancel</Button>
+                            <Button onClick={handleSave}>Save</Button>
+                        </DialogActions>
+                    </Dialog>
+            }
+        </>
+
+    )
+
+}
+
+export default AddTraining
--- src/components/TrainingsList.jsx
+++ src/components/TrainingsList.jsx
@@ -5,6 +5,7 @@ import "ag-grid-community/styles/ag-theme-material.css";
 import { Stack, Button, Snackbar } from "@mui/material";
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import AddTraining from './AddTraining.jsx'
 
 const TrainingsList = () => {
 
@@ -39,6 +40,31 @@ const TrainingsList = () => {
         }
     }
 
+    const addTraining = async (training) => {
+
+        try {
+            const response = await fetch("https://customerrestservice-personaltraining.rahtiapp.fi/api/trainings",
+                { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(training) });
+
+            if (response.ok) {
+                setMsgSnackbar("Added new training");
+                setOpenSnackbar(true);
+                getTrainings();
+            }
+
+            else {
+                setMsgSnackbar("Failed to add training");
+                setOpenSnackbar(true);
+            }
+
+        }
+
+        catch (error) {
+            console.error(error);
+        }
+
+    }
+
     const deleteTraining = async (params) => {
 
         if (confirm("Are you sure you want to delete this training?")) {
@@ -71,26 +97,28 @@ const TrainingsList = () => {
     }, [])
 
     return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Stack justifyContent="center" alignItems="center">
 
-        <Stack justifyContent="center" alignItems="center">
+                <h1>Trainings</h1>
 
-            <h1>Trainings</h1>
+                {
+                    loading ? <p>Loading trainings...</p> :
+                        <>
+                            <AddTraining addTraining={addTraining} />
 
-            {
-                loading ? <p>Loading trainings...</p> :
-                    <>
-                        <div className="ag-theme-material" style={{ width: 1620, height: 1000 }}>
-                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <div className="ag-theme-material" style={{ width: 1620, height: 1000 }}>
+
                                 <AgGridReact rowData={trainings} columnDefs={colDefs} pagination={true} paginationPageSize={20} />
-                            </LocalizationProvider>
-                        </div>
-                        <Snackbar open={openSnackbar} message={msgSnackbar} autoHideDuration={3000} onClose={() => setOpenSnackbar(false)}>
-                        </Snackbar>
-                    </>
-            }
 
-        </Stack>
+                            </div>
+                            <Snackbar open={openSnackbar} message={msgSnackbar} autoHideDuration={3000} onClose={() => setOpenSnackbar(false)}>
+                            </Snackbar>
+                        </>
+                }
 
+            </Stack>
+        </LocalizationProvider>
     )
 
 }
